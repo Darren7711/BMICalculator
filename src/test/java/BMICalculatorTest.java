f2b284367c7a135e0163c7d8c93f82df5aef8088
andrey.bondalovsky <andrey.bondalovsky@kyriba.com>
2021-01-14 22:18:18 +0300

    Update BMICalculatorTest

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -1,40 +1,26 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BMICalculatorTest extends BasedTest{
+    //webdriver == браузер
+    //webelement == элемент на странице
 
     @Test
-    public void starvingCategoryShouldBeShown(){
-        //webdriver == браузер
-        //webelement == элемент на странице
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://healthunify.com/bmicalculator/");
+    public void starvationCategoryShouldBeShown(){
 
         driver.findElement(By.name("wg")).sendKeys("50");
         driver.findElement(By.name("ht")).sendKeys("183");
         driver.findElement(By.name("cc")).click();
 
         String category = driver.findElement(By.name("desc")).getAttribute("value");
-        Assert.assertEquals(category,"Your category is Starving");
+        Assert.assertEquals(category,"Your category is Starvation");
         String index = driver.findElement(By.name("si")).getAttribute("value");
-        Assert.assertEquals(index,"23.89");
+        Assert.assertEquals(index,"14.93");
     }
 
     @Test
     public void normalCategoryShouldBeShown(){
-        //webdriver == браузер
-        //webelement == элемент на странице
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://healthunify.com/bmicalculator/");
 
         driver.findElement(By.name("wg")).sendKeys("80");
         driver.findElement(By.name("ht")).sendKeys("183");
@@ -47,27 +33,28 @@ public class BMICalculatorTest extends BasedTest{
     }
 
     @Test
-    public void ObeseCategoryShouldBeShown(){
-        //webdriver == браузер
-        //webelement == элемент на странице
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+    public void overweightCategoryShouldBeShown(){
 
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://healthunify.com/bmicalculator/");
+        driver.findElement(By.name("wg")).sendKeys("100");
+        driver.findElement(By.name("ht")).sendKeys("183");
+        driver.findElement(By.name("cc")).click();
 
-        driver.findElement(By.name("wg")).sendKeys("142");
+        String category = driver.findElement(By.name("desc")).getAttribute("value");
+        Assert.assertEquals(category,"Your category is Overweight");
+        String index = driver.findElement(By.name("si")).getAttribute("value");
+        Assert.assertEquals(index,"29.86");
+    }
+
+    @Test
+    public void obeseCategoryShouldBeShown(){
+
+        driver.findElement(By.name("wg")).sendKeys("120");
         driver.findElement(By.name("ht")).sendKeys("183");
         driver.findElement(By.name("cc")).click();
 
         String category = driver.findElement(By.name("desc")).getAttribute("value");
         Assert.assertEquals(category,"Your category is Obese");
         String index = driver.findElement(By.name("si")).getAttribute("value");
-        Assert.assertEquals(index,"42.4");
-    }
-
-    @Test
-    public void test(){
-        //opt1
-
+        Assert.assertEquals(index,"35.83");
     }
 }
